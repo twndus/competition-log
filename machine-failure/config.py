2c8a092872805db90f4066a65781b5cf019f63ea
twndus <twndus1362@gmail.com>
2023-06-26 09:31:28 +0900

    local works

--- machine-failure/config.py
+++ machine-failure/config.py
@@ -1,13 +1,13 @@
 import os
 
-data_dir='/Users/leejuyeon/Downloads/playground-series-s3e17'
+data_dir='/home/leejuyeon/dataset/kaggle/machine-failures'
 # args
 args = {
     'train_path': os.path.join(data_dir, 'train.csv'),
     'test_path': os.path.join(data_dir, 'test.csv'),
     'submission_path': os.path.join(data_dir, 'sample_submission.csv'),
     'task': 'classification',
-    'modelname': 'knn',
+    'modelname': 'mlp',
     'metric': 'auc',
     #'xgboost' #'mlp_keras' #'gbm'#'mlp' #'rf' #'svc' #'logistic' #'knn'
     'dataname': 'machine',
